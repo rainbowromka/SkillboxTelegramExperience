a3fca6a16e942306a316b6d8cecced66d020db93
rainbow.romka <broken_mouse@mail.ru>
2018-10-31 23:00:02 +0800

    эксперименты с телеграмом

---
+++ src/loader.java
@@ -0,0 +1,9 @@
+import org.javagram.TelegramApiBridge;
+
+import java.io.IOException;
+
+public class loader {
+    public static void main(String[] args) throws IOException {
+        TelegramApiBridge bridge = new TelegramApiBridge("149.154.167.50:443", 638472, "26a72d210d6cbe418f2d08803c496c51");
+    }
+}
